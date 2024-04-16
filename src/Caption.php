845e991e26dec9f24882d9c4ca21f200c95bad77
Wongyip <wongyip@outlook.com>
2024-04-16 15:40:26 +0800

    Updated Caption tag.

--- src/Caption.php
+++ src/Caption.php
@@ -2,15 +2,48 @@
 
 namespace Wongyip\HTML;
 
+use Exception;
+
 /**
  * A table caption.
+ *
+ * @method Caption sideBottom() Set caption-side CSS property's value to "bottom".
+ * @method Caption sideTop() Set caption-side CSS property's value to "top".
+ * @method Caption sideInherit() Set caption-side CSS property's value to "inherit".
+ * @method Caption sideInitial() Set caption-side CSS property's value to "initial".
+ *
  */
 class Caption extends TagAbstract
 {
+    const DEFAULT_SIDE = 'top';
+
+    /**
+     * @var array|string[]
+     */
+    private static array $sides = ['top', 'bottom', 'initial', 'inherit'];
     /**
      * @var string
      */
     protected string $tagName = 'caption';
+
+    /**
+     * @param string $name
+     * @param array $arguments
+     * @return bool|Caption|TagAbstract|null
+     * @throws Exception
+     */
+    public function __call(string $name, array $arguments)
+    {
+        if (preg_match("/^side([A-Z][a-z]+)$/", $name, $matches)) {
+            $setter = strtolower($matches[1]);
+            if (in_array($setter, static::$sides)) {
+                $this->styleProperty('caption-side', $setter);
+                return $this;
+            }
+            // Let it go.
+        }
+        return parent::__call($name, $arguments);
+    }
 
     /**
      * @inheritdoc
@@ -31,9 +64,13 @@ class Caption extends TagAbstract
     {
         // @todo validate $side.
 
+        $side = $side
+            ? (in_array($side, static::$sides) ? $side : static::DEFAULT_SIDE)
+            : static::DEFAULT_SIDE;
+
         return Caption::make()
             ->contents($contents)
-            ->styleAppend("caption-side: $side");
+            ->styleProperty('caption-side', $side);
     }
 
 }
