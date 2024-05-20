a34df40d00b2effd04ff9a91d42cf6256460f11a
Wongyip <wongyip@outlook.com>
2024-05-20 11:32:21 +0800

    Added <label> tag.

---
+++ src/Label.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Wongyip\HTML;
+
+/**
+ * A basic implementation of a "\<label>" tag.
+ *
+ * @method string|static for(string $set = null)
+ */
+class Label extends TagAbstract
+{
+    /**
+     * @var string
+     */
+    protected string $tagName = 'label';
+
+    /**
+     * @inheritdoc
+     */
+    public function addAttrs(): array
+    {
+        return ['for'];
+    }
+
+    /**
+     * Create an \<label> tag.
+     *
+     * @param string $for
+     * @return static
+     */
+    public static function create(string $for): static
+    {
+        return Label::make()->attributes(compact('for'));
+    }
+
+}
